916fe31cbefb2e488b3abbc38da4883d8cef0ab6
natthaphong <s6704062611336@email.kmutnb.ac.th>
2024-11-28 14:46:36 +0700

    ข้อ
    จากข้อ 3 เปลี่ยนให้รับจากคีย์บอร์ดด้วยรูปแบบดังนี้
    n1
    n2

--- lab1minx.c
+++ lab1minx.c
@@ -4,8 +4,11 @@ int main() {
   int num1;
   int num2;
   
-  printf("input");
-  scanf("%d %d",&num1, &num2);
+  printf("input num1");
+  scanf("%d/n",&num1);
+  
+  printf("input num2");
+  scanf("%d/n",&num2);
   
   if(num1 > num2){
   		int total = num1 - num2;
