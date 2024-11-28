30e4f706eb84760f58c2c790b3580aa2b3210570
natthaphong <s6704062611336@email.kmutnb.ac.th>
2024-11-28 14:39:41 +0700

    ข้อ 2
    จากข้อ 1 ปรับแก้โค้ดให้รับข้อมูลจากคีย์บอร์ด โดยให้แสดงข้อความและทำการป้อนข้อมูล ดังนี้
    n1=<ป้อนจำนวนเต็มค่าแรก>
    n2=<ป้อนจำนวนเต็มค่าที่สอง>
    
    และ ให้แจ้งว่าค่าที่มากกว่า มีค่ามากกว่าค่าที่น้อยกว่าจำนวนเท่าใด

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  int num1 = 10;
-  int num2 = 15;
-  if (num1>num2) {
-  	printf("num1 > num2");
-  }else{
-  	printf("num2 < num1");
+  int num1;
+  int num2;
+  
+  printf("input num1\n");
+  scanf("%d/n",&num1);
+  
+  printf("input num2\n");
+  scanf("%d/n",&num2);
+  
+  if(num1 > num2){
+  		int total = num1 - num2;
+  		printf("num1 > num2 = %d",total);
+	}else{
+  		int total = num2 - num1;
+  		printf("num1 < num2 = %d",total);
   }
 }
