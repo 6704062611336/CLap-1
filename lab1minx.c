1e52a64032e0bf7c96698baebafe3d9ce8669981
natthaphong <s6704062611336@email.kmutnb.ac.th>
2024-11-28 14:16:23 +0700

    ข้อ 1 เสร็จแล้วครับ
    ข้อ 1
    1. กำหนดค่าตัวแปร 2 ค่าเก็บจำนวนเต็มที่ไม่เท่ากัน
    2. เปรียบเทียบว่าค่าใดมากกว่ากัน

---
+++ lab1minx.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int num1 = 10;
+  int num2 = 15;
+  if (num1>num2) {
+  	printf("num1 > num2");
+  }
+  if (num2>num1) {
+  	printf("num2 < num1");
+  }
+    return 0;
+}
